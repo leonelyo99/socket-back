02f4bf2b7a2a8c016a9d9b16bd6cc32b26240606
leonel mazzan <leonelmazzan@gmail.com>
2021-09-11 16:40:14 -0300

    add comments

--- routes/socket.js
+++ routes/socket.js
@@ -8,22 +8,37 @@ exports.connectSocket = (server) => {
     },
   });
 
-  console.log("listening ",process.env.PORT);
+  console.log("listening ", process.env.PORT);
 
   io.sockets.on("connection", function (socket) {
     console.log("connection to the socket");
 
+    /**
+     * Listen to the messages sent by the front
+     */
     socket.on("new-message", (data, callback) => {
       incomingMessage(data).then((resp) => {
         if (!resp.error) {
+          /**
+           * io
+           * emit the message and the event has the id of the room
+           */
           io.emit(`message-${data.room}`, resp.data);
           resp.usersToNotify.forEach((user) => {
+            /**
+             * io
+             * Send notifications to all users who need to be notified
+             */
             io.emit(`notification-${user.id}`, {
               error: false,
               data: resp.user,
             });
           });
         } else {
+          /**
+           * socket
+           * Send the error to the user who is connected to the socket
+           */
           socket.emit(`error`, resp);
         }
       });
